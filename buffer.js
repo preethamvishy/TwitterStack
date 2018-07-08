77326aa9ed0bc78d14f3e3e42ce63ab2f97cb3e5
Preetham Viswanathan <preethamvishy10@gmail.com>
2018-07-08 23:53:02 +0530

    Added Buffer API endpoints

---
+++ buffer.js
@@ -0,0 +1,78 @@
+var request = require('request');
+const Buffer = require('buffer-js');
+var config = require('./config');
+
+const bufferAPIUrl = 'https://api.bufferapp.com/1'
+const opts = {
+    access_token: config.bufferAccessToken
+};
+var Profiles = new Buffer.Profiles(opts);
+var Updates = new Buffer.Updates(opts);
+
+
+export default {
+    profiles: function(req, res) {
+        Profiles.all((body) => {
+            res.json(body)
+        });
+    },
+    profile: function(req, res) {
+        var profOpts = opts;
+        profOpts.profile_id = req.params.id;
+        Profiles.get((body) => {
+            res.json(body)
+        });
+    },
+    
+    pendingUpdates: function(req, res) {
+        var updateOpts = opts;
+        updateOpts.profile_id = req.params.id;
+        console.log(updateOpts)
+        Profiles.pendingUpdates(updateOpts, (body) => {
+            res.json(body)
+        });
+    },
+
+    createUpdates: function (req, res) {
+        var createOpts = {
+            access_token: config.bufferAccessToken,
+            profile_ids: req.body.profile_id,
+            text: req.body.text ||  '',
+            now: req.body.now || 'false',
+            top: req.body.top || 'false',
+        }
+        if (req.body.media !== undefined)
+            createOpts.media = {
+                photo: req.body.media
+            }
+        if (req.body.scheduled_at !== undefined)
+            createOpts.scheduled_at = req.body.scheduled_at
+
+        // var options = {
+        //     method: req.method,
+        //     url: `${bufferAPIUrl}/updates/create.json`,
+        //     headers:
+        //     {
+        //         'Cache-Control': 'no-cache',
+        //         'Content-Type': 'application/x-www-form-urlencoded'
+        //     },
+        //     form: createOpts
+        // };
+
+        // request(options, function (error, response, body) {
+        //     if (error) throw new Error(error);
+        //     res.json(body);
+        // });
+        Updates.create(createOpts, (body) => {
+            res.json(body)
+        })
+    },
+
+    shuffleUpdates: function(req, res) {
+        var shuffleOpts = opts;
+        shuffleOpts.profile_id = req.params.id;
+        Profiles.shuffleUpdates(shuffleOpts, (body) => {
+            res.json(body)
+        });
+    },
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 import twitter from './twitter';
+import buffer from './buffer';
 
 var app = express();
 
@@ -13,10 +14,16 @@ const PORT = 5555;
 
 app.get('/api/authorize', twitter.authorize);
 app.get('/api/search/:query/:max?', twitter.search);
-app.get('/api/user/:screenname/:max?', twitter.user);
+app.get('/api/user/:screenname/tweets/:max?', twitter.user);
+// app.get('/api/user/:screenname/info', twitter.userInfo);
 app.get('/api/list/:screenname/:slug/:max?', twitter.list);
 app.get('/api/favs/:screenname/:max?', twitter.favs);
 app.get('/api/id/:id', twitter.id);
 
+app.get('/api/buffer/profiles', buffer.profiles);
+app.get('/api/buffer/profile/:id', buffer.profile);
+app.get('/api/buffer/pending/:id', buffer.pendingUpdates);
+app.post('/api/buffer/create/', buffer.createUpdates);
+app.get('/api/buffer/shuffle/:id', buffer.shuffleUpdates);
 
 app.listen(PORT);

5ac0fb1cb98386acb3f04b065711c29311b6f134
Preetham Viswanathan <preethamvishy10@gmail.com>
2018-07-08 05:47:47 +0530

    Express routes for twitter GET

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+import twitter from './twitter';
 
 var app = express();
 
@@ -8,5 +10,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 const PORT = 5555;
+
+app.get('/api/authorize', twitter.authorize);
+app.get('/api/search/:query', twitter.search);
+app.get('/api/user/:screenname', twitter.user);
+app.get('/api/list/:screenname/:slug', twitter.list);
+app.get('/api/favs/:screenname', twitter.favs);
+app.get('/api/id/:id', twitter.fetchById);
+
 
 app.listen(PORT);
